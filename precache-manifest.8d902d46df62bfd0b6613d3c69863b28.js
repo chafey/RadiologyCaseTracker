cffa6666dd9c89381cbddccc0ae90ab687a40f8e
chafey <chafey@gmail.com>
2020-06-23 15:41:54 +0000

    Deploying to gh-pages from  @ 1e2123faf7cb14b3f9b11c6562c006f819a694b9 🚀

---
+++ precache-manifest.8d902d46df62bfd0b6613d3c69863b28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282f1a6319e35fb68d0b6d0e7ba349ae",
+    "url": "/RadiologyCaseTracker/index.html"
+  },
+  {
+    "revision": "59987caf94e9ad9cada3",
+    "url": "/RadiologyCaseTracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aaed2c13563e18ab1e0a",
+    "url": "/RadiologyCaseTracker/static/css/main.29b0924c.chunk.css"
+  },
+  {
+    "revision": "59987caf94e9ad9cada3",
+    "url": "/RadiologyCaseTracker/static/js/2.a58c3fc3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/RadiologyCaseTracker/static/js/2.a58c3fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaed2c13563e18ab1e0a",
+    "url": "/RadiologyCaseTracker/static/js/main.66558ec4.chunk.js"
+  },
+  {
+    "revision": "e6898517fff3d99e3bea",
+    "url": "/RadiologyCaseTracker/static/js/runtime-main.062ab603.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RadiologyCaseTracker/precache-manifest.71a129d230935de3564755318834254b.js"
+  "/RadiologyCaseTracker/precache-manifest.8d902d46df62bfd0b6613d3c69863b28.js"
 );
 
 self.addEventListener('message', (event) => {
